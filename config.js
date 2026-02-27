e33db1eb6db656e5cccfdc559867b0c56891f200
any911104-ops <any911104@gmail.com>
2026-02-27 11:24:20 +0900

    Update wedding details in config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ export const GROOM_NAME = '김창기';
 export const BRIDE_NAME = '안윤정';
 export const WEDDING_LOCATION = '위더스 안양 9층 메리엘홀';
 
-export const GROOM_NAME = "김학묵";
+export const GROOM_NAME = "김창기";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_FATHER_NAME = "박미근";
+export const GROOM_FATHER_NAME = "김학묵";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_MOTHER_NAME = "김창기";
+export const GROOM_MOTHER_NAME = "박미근";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
-export const BRIDE_NAME = "안병인";
+export const BRIDE_NAME = "안윤정";
 export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const BRIDE_FATHER_NAME = "장혜랑";
+export const BRIDE_FATHER_NAME = "안병인";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const BRIDE_MOTHER_NAME = "안윤정";
+export const BRIDE_MOTHER_NAME = "장혜랑";
 export const BRIDE_MOTHER_ACCOUNT_NUMBER = "케이뱅크 1001-71-110178";
