7605edfc976f57b2371b951f24d4e560e4632abf
any911104-ops <any911104@gmail.com>
2026-02-27 09:58:11 +0900

    Update location.jsx

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -44,54 +44,28 @@ const Location = () => {
   // 카카오 맵 불러오기
 
   // <!-- 3. 실행 스크립트 -->
-  const executeScript = () => {
-    const scriptTag = document.createElement("script");
-    const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1772094638308",
-    "key" : "i2gbi4qzzwx",
-    "mapWidth" : "640",
-    "mapHeight" : "360"
-  }).render();`);
-    scriptTag.appendChild(inlineScript);
-    document.body.appendChild(scriptTag);
-  };
 
   // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
   // document.write 문제가 발생해서 해당 파일을 직접 가져온다음 수정했음
-  const InstallScript = () => {
-    (function () {
-      let c = window.location.protocol === "https:" ? "https:" : "http:";
-      let a = "16137cec";
-
-      if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
-        return;
-      }
-      window.daum = window.daum || {};
-      window.daum.roughmap = {
-        cdn: a,
-        URL_KEY_DATA_LOAD_PRE: c + "//t1.daumcdn.net/roughmap/",
-        url_protocal: c,
-      };
-      let b =
-        c +
-        "//t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/" +
-        a +
-        "/roughmapLander.js";
-
-      // document.write -> doumnet.body.append로 수정
-      const scriptTag = document.createElement("script");
-      scriptTag.src = b;
-      document.body.append(scriptTag);
-      scriptTag.onload = () => {
-        executeScript();
-      };
-    })();
-  };
-
+ 
 useEffect(() => {
-  InstallScript();
-}, []);
+  const script = document.createElement("script");
+  script.src =
+    "https://t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/roughmapLoader.js";
+  script.async = true;
 
+  document.body.appendChild(script);
+
+  script.onload = () => {
+    new window.daum.roughmap.Lander({
+      timestamp: "1772094638308",
+      key: "i2gbi4qzzwx",
+      mapWidth: "640",
+      mapHeight: "360",
+    }).render();
+  };
+}, []);
+ 
   return (
     <Wrapper>
       <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
